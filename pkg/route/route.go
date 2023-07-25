4e983cd907ac98d2a1797ad260f42dc5e0b99b5d
Unknown <a@x.com>
2023-07-25 12:35:18 +0530

    correct typo

--- pkg/route/route.go
+++ pkg/route/route.go
@@ -11,7 +11,7 @@ var RegisterNewroute = func(router *mux.Router) {
 	router.HandleFunc("/immigrants", controller.GetImmigrants).Methods("GET")
 	//	router.HandleFunc("/immigrants/{passportno}", controller.GetImmigrant).Methods("GET")
 	router.HandleFunc("/immigrants/approved", controller.GetApprovedImmigrants).Methods("GET")
-	router.HandleFunc("/immigrants/peding", controller.GetPendingImmigrants).Methods("GET")
+	router.HandleFunc("/immigrants/pending", controller.GetPendingImmigrants).Methods("GET")
 	router.HandleFunc("/immigrants/rejected", controller.GetRejectedImmigrants).Methods("GET")
 	router.HandleFunc("/immigrants/accept/{passportno}", controller.AcceptImmigrant).Methods("PUT")
 }
